8a171c83bd30d843c8021d3c65dc9d227df6e646
Gabriel Engle <gab.engle.dev@gmail.com>
2024-12-02 13:39:09 -0300

    Delete src directory

--- src/components/banner/index.tsx
+++
@@ -1,6 +0,0 @@
-export default function Banner({ text }: { imgSrc?: string, text: string }): React.ReactNode {
-  return (
-    <div className="bg-slate-600 h-96 flex justify-center items-center">
-      <h1 className="text-2xl font-bold text-white">{text}</h1>
-    </div>)
-}
--- src/components/header/headerButton.tsx
+++
@@ -1,6 +0,0 @@
-export default function HeaderButton({ children, className = '' }: {
-  children: React.ReactNode,
-  className?: string | undefined
-}) {
-  return <li className={"hover:text-yellow-400 cursor-pointer".concat(" ", className)}>{children}</li>
-}
