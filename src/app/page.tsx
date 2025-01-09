8ccb1e9735deafebcbd719d7a1ef7f9b637c5705
Iago Rosa de Oliveira <125699322+iagorosa28@users.noreply.github.com>
2025-01-09 23:33:42 +0000

    Botando automatico o carrossel do projetos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 "use client";
+
+import Banner from "@/components/banner";
 import Header from "@/components/header";
-import Carrosel from "@/components/banner";
 import { IoCalendarOutline } from "react-icons/io5";
 import { GiReceiveMoney } from "react-icons/gi";
 import News from "@/components/news";
-import CalendarEvents from '@/components/calendar';
 import HomeSideButton from "@/components/homeSideButton";
 import QRcode from "@/components/qrcode";
+import Projetos from "@/components/projetos";
+
 
 export default function Home() {
 
   return (
     <div>
       <Header />
-      <Carrosel />
+      <Banner/>
       <div className="grid grid-cols-6 my-8 px-24">
         <div className="col-span-1">
           <h3 className="bg-yellow-500 text-white m-auto font-bold uppercase rounded-full w-fit px-16 py-1">dacc news</h3>
@@ -33,24 +35,22 @@ export default function Home() {
 
           <div className="text-center my-16 flex flex-col gap-5">
             <h4 className="font-bold text-lg">Projetos em andamento</h4>
+
             <span>Para saber mais sobre esses e outros <span className="font-bold">projetos</span> ativos no diretório <span className="text-yellow-500 font-bold cursor-pointer">clique aqui</span> </span>
 
-            <div className="flex justify-center items-center gap-8">
-              <div className="bg-slate-300 size-56  font-bold cursor-pointer flex justify-center items-center">
-                Sociedade da Coruja</div>
-              <div className="bg-slate-300 size-56 font-bold cursor-pointer flex justify-center items-center">
-                Arena FEI</div>
-              <div className="bg-slate-300 size-56 my-8 font-bold cursor-pointer flex justify-center items-center">
-                Dev Week</div>
+            {/* Substituindo a lista pelos projetos no carrossel */}
+            <div className="mt-8">
+              <Projetos />
             </div>
           </div>
 
         </div>
         <div className="col-span-1 flex flex-col items-center gap-8">
-
-          <HomeSideButton icon={<IoCalendarOutline size={32} />} content={<CalendarEvents />} />
-          <HomeSideButton icon={<GiReceiveMoney size={32} />} content={<QRcode />} />
-
+          <div className="bg-blue-950 rounded-full size-12 flex justify-center items-center cursor-pointer text-white hover:bg-blue-900 duration-150 ">
+            <IoCalendarOutline size={32} />
+          </div>
+          <HomeSideButton icon={<GiReceiveMoney size={32}/>} content={<QRcode/>}/>
+          
         </div>
       </div>
     </div>
