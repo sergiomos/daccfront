9e1666490803469539c21a408c84d5cdb19b36bd
Sergio Oliveira <sergiomos2010@gmail.com>
2025-01-19 16:15:04 -0300

    colocando o nome na funcao do componente

--- src/components/divisor/index.tsx
+++ src/components/divisor/index.tsx
@@ -1,8 +1,12 @@
-const Barra = ({ color }) => (
+type BarraProps = {
+  color: string
+}
+
+const Barra = ({ color }: BarraProps) => (
   <span className={`px-28 py-2 rounded-full ${color}`}></span>
 )
 
-export default () => (
+const Divisor = () => (
   <div className="flex justify-between min-h-fit px-8 py-4">
     <Barra color="bg-yellow-400" />
     <Barra color="bg-blue-900" />
@@ -11,3 +15,5 @@ export default () => (
     <Barra color="bg-yellow-400" />
   </div>
 )
+
+export default Divisor
