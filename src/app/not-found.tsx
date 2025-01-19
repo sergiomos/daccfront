9f7ab1a22182e6e24d659bee0392662ae3294976
Sergio Oliveira <sergiomos2010@gmail.com>
2025-01-19 16:29:04 -0300

    criando pagina not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+
+const PageNotFound = () => (
+  <div>
+    <Header />
+    <h1 className="font-bold text-2xl text-center py-60">Opps, página não encontrada</h1>
+  </div>
+)
+
+export default PageNotFound;
